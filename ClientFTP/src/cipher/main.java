eab47786484e282998f778357781e7aeb2c02648
Heradocles <heradocles@gmail.com>
2015-11-20 13:53:01 -0430

    Patria

---
+++ ClientFTP/src/cipher/main.java
@@ -0,0 +1,9 @@
+package cipher;
+
+//Created by Heradocles and Mendez
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ServerFTP/src/cipher/main.java
@@ -0,0 +1,10 @@
+package cipher;
+
+
+//Created by Heradocles and Mendez
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
