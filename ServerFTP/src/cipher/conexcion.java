553494a4d0ea7f375ebeda51ce74e5ebef54d5b8
Heradocles <heradocles@gmail.com>
2015-11-22 23:28:53 -0430

    ermio

--- ServerFTP/src/cipher/conexcion.java
+++ ServerFTP/src/cipher/conexcion.java
@@ -12,7 +12,7 @@ public class conexcion {
 
         String help = "La lista de comandos es la siguiente: \n\n Put ( put 'direccion del archivo') Esto enviara un archivo al servidor. \n Get (get 'direccion del archivo) Esto recibira un archivo del servidor. \n Quit, para salir de la aplicacion \n Delete (delete 'nombre del archivo), elimina un archivo del servidor \n List (list), se usa para ver la lista de archivos disponibles en el servidor \n";
 
-        Socket SocketCliente = null;
+        Socket ElSocket = null;
         ServerSocket Server;
 
         try {
@@ -20,7 +20,7 @@ public class conexcion {
             System.out.println("\n>Iniciando servidor.");
             Server=new ServerSocket(9000);
             System.out.println("\n>Esperando conexcion de un cliente.");
-            SocketCliente=Server.accept();
+            ElSocket=Server.accept();
             System.out.println("Conexcion con cliente exitosa.");
 
         } catch (IOException e) {
